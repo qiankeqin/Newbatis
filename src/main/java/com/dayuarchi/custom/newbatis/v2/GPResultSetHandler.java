ca2bfc568ff5d20fa052ae9447641845589c4657
qiankeqin <qiankeqin@51xf.cn>
2019-07-24 13:42:52 +0800

    mybatis 2.0 resultSetHandler

--- src/main/java/com/dayuarchi/custom/newbatis/v2/GPResultSetHandler.java
+++ src/main/java/com/dayuarchi/custom/newbatis/v2/GPResultSetHandler.java
@@ -3,6 +3,8 @@ package com.dayuarchi.custom.newbatis.v2;
 import org.apache.ibatis.reflection.factory.DefaultObjectFactory;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,7 +20,7 @@ public class GPResultSetHandler {
         this.configuration = configuration;
     }
 
-    public <E> E handle(PreparedStatement preparedStatement, GPMapperData mapperData) throws SQLException {
+    public <E> E handle(PreparedStatement preparedStatement, GPMapperData mapperData) throws SQLException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         //使用ObjectFactory常见类实例
         Object resultObj = new DefaultObjectFactory().create(mapperData.getType());
         ResultSet resultSet = preparedStatement.getResultSet();
@@ -31,7 +33,25 @@ public class GPResultSetHandler {
         return (E)resultObj;
     }
 
-    private void setValue(Object resultObj, Field field, ResultSet resultSet, int i) {
+    private void setValue(Object resultObj, Field field, ResultSet resultSet, int i) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, SQLException {
+        //拿到对象中的set属性的方法
+        Method setMethod = resultObj.getClass().getMethod("set" + upperCapital(field.getName()), field.getType());
+        setMethod.invoke(resultObj,getResult(field,resultSet));
+    }
 
+    private Object getResult(Field field, ResultSet resultSet) throws SQLException {
+        //Mybatis这里用了typeHandler
+        Class<?> type = field.getType();
+        if(Integer.class == type){
+            return resultSet.getInt(field.getName());
+        }
+        if(String.class == type){
+            return resultSet.getString(field.getName());
+        }
+        return null;
+    }
+
+    private String upperCapital(String name) {
+        return name.substring(0,1).toUpperCase()+name.substring(1);
     }
 }
